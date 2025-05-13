d570d40e652820750eb331a35e413ae68a41fa4f
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-05-13 16:48:06 +0530

    added mongodb file and add route to add autoRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './config/mongoDB.js';
+import audioRoutes from './routes/audioRoutes.js'
 
 
 dotenv.config();
 
 
+
 const app = express()
+connectDB()
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,6 +18,8 @@ app.use(cors())
 app.use(express.json());
 
 //Routes
+
+app.unsubscribe('/api/audio',audioRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
