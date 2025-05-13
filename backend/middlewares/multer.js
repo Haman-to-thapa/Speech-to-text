0fc37d4ae65681506611ae2291f87b15f05b0173
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-05-13 16:47:21 +0530

    added file and video configuration to added in backend to multer

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,14 +1,35 @@
-import multer from "multer";
-
+import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
-  filename: (req,file, cb) => {
-    let fileExt = file.originalname.split("-").pop();
-
-    const fileName = `${new Date().getTime()}.${fileExt}`;
-
-    cb(null, fileName)
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); 
   },
-})
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname); // gets .mp3 or .mpeg
+    const fileName = `${Date.now()}${fileExt}`;
+    cb(null, fileName);
+  },
+});
 
 
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype !== "audio/mpeg" && file.mimetype !== "audio/mp3") {
+    req.fileValidationError = "File type must be audio/mp3 or audio/mpeg";
+
+    return  cb(null, false);
+  } else {
+  cb(null, true)
+     
+  }
+};
+
+
+const upload = multer({
+  storage,
+  fileFilter
+}).single('audio');
+
+
+export default upload;
